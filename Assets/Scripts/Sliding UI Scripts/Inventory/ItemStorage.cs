74ee320226a93c8a14e70c351789254f4058d3fb
Mac Burton <macburton1000@gmail.com>
2022-06-18 20:10:51 -0300

    Added switch to display how many glasses of specific type are equiped. Pending buffs and PPE

--- Assets/Scripts/Sliding UI Scripts/Inventory/ItemStorage.cs
+++ Assets/Scripts/Sliding UI Scripts/Inventory/ItemStorage.cs
@@ -25,21 +25,83 @@ public class ItemStorage : MonoBehaviour
     public void PushItem(GameObject item)
     {
         Items.Add(item);
-        string buffText = "";
-        foreach (GameObject glasses in Items)
-        {
-            buffText += glasses.name + "\n";
-        }
-        itemStorage.text = buffText;
+        UpdateList();
     }
 
     public void PopItem(GameObject item)
     {
         Items.Remove(item);
+        UpdateList();
     }
 
     public List<GameObject> GetItems()
     {
         return Items;
+    }
+
+    public void UpdateList()
+    {
+        string buffText = "";
+        int shades = 0;
+        int purple = 0;
+        int alien = 0;
+        int fire = 0;
+        int groncho = 0;
+        int shutter = 0;
+        foreach (GameObject glasses in Items)
+        {
+            switch (glasses.name)
+            {
+                case "Shades":
+                    shades++;
+                    break;
+                case "Purple":
+                    purple++;
+                    break;
+                case "Alien":
+                    alien++;
+                    break;            
+                case "Fire":
+                    fire++;
+                    break;
+                case "Groncho":
+                    groncho++;
+                    break;
+                case "Shutter":
+                    shutter++;
+                    break;
+            }
+        }
+        if (shades > 0)
+        {
+            // Increase speed
+            buffText += shades + " Shades \n";
+        }
+        if (purple > 0)
+        {
+            // Increase jump
+            buffText += purple + " Purple \n";
+        }
+        if (alien > 0)
+        {
+            // Increase slide speed
+            buffText += alien + " Alien \n";
+        }
+        if (fire > 0)
+        {
+            // Increase slide distance
+            buffText += fire + " Fire \n";
+        }
+        if (groncho > 0)
+        {
+            // Spawn less targets(?)
+            buffText += groncho + " Groncho \n";
+        }
+        if (shutter > 0)
+        {
+            // Shave 5s off timer
+            buffText += shutter + " Shutter";
+        }
+        itemStorage.text = buffText;
     }
 }
